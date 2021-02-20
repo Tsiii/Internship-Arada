d5810858ad2db7c771a8be9380b12d32eb641b1a
Tsion <39256779+Tsiii@users.noreply.github.com>
2021-02-20 10:12:25 +0300

    sat 4oclock

--- Admin/index.php
+++ Admin/index.php
@@ -51,8 +51,7 @@ while ($row1=mysqli_fetch_array($result)) {
     </div>
 
     <!-- Content Row -->
-    <div class="row">
- 
+    <div class="row"> 
         <!-- Earnings (Monthly) Card Example -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-success shadow h-100 py-2">
@@ -138,8 +137,7 @@ while ($row1=mysqli_fetch_array($result)) {
     </div>
  
     <!-- Content Row -->
-    <div class="row">
-
+    <div class="row"> 
         <!-- IT Employee Report Column -->
         <div class="col-lg-12 mb-3">  
             <!-- Project Card Example -->
@@ -149,7 +147,72 @@ while ($row1=mysqli_fetch_array($result)) {
                 </div>
 
                 <div class="card-body border-left-dark"> 
-                    <div class="table-responsive">  
+                
+                <div class="text-lg font-weight-bold text-dark text-uppercase mb-1 "> Maintained</div> 
+                    
+                    <!-- Maintained Request Table Card -->
+                    <div class="table-responsive"> 
+                        <table class='table table-bordered'> 
+                            <thead>  
+                                <tr>                  
+                                    <th> Service Type </th> 
+                                    <th> Quantity </th>   
+                                    <th> Unit Cost</th>
+                                    <th> Total Cost</th>  
+                                </tr>
+                            </thead>
+                            
+                            <tbody>  
+                                <?php  
+                                $service = mysqli_query($db, "SELECT COUNT(ID), Services FROM maintenancerequest WHERE Request_Status = 'MAINTAINED' GROUP BY Services HAVING COUNT(ID) > 0; ");
+                                                                
+                                $total_cost = 0;  
+                                $total_quantity = 0 ;               
+                                while ($rowq=mysqli_fetch_assoc($service)) {  
+
+                                    $quantity  = $rowq["COUNT(ID)"];  ?> 
+                                    
+                                    <tr>   
+                                        <td> <?php echo $rowq["Services"]; ?>  </td> 
+                                        <td> <?php echo $rowq["COUNT(ID)"]; ?>  </td>
+                                        
+                                        <?php 
+
+                                        $services2= mysqli_query($db,"SELECT Cost FROM services WHERE Services = '$rowq[Services]' ");
+                                        while ($rowq2=mysqli_fetch_assoc($services2)) {
+                                                
+                                            $unitCost = 1 * $rowq2["Cost"] ;
+                                            $sumCost = $quantity * $rowq2["Cost"] ;
+
+                                            $total_quantity += $quantity; 
+                                            $total_cost += $sumCost;
+                                                 
+                                            ?>
+
+                                            <td> <?php echo $unitCost; ?>  </td> 
+                                            <td> <?php echo $sumCost; ?>  </td>  
+                                        
+                                        <?php   
+                                        }
+                                        ?>  
+                                    </tr>
+                                    <?php
+                                }     
+                                ?>   
+                            </tbody>
+                        </table>
+
+                        <div class="text-dark mt-2 mb-4 float-right ">
+                            <i class="text-lg alert alert-success ">Total Item: <?php echo $total_quantity;?></i>  
+                            <i class="text-lg alert alert-warning ">Total Cost: <?php echo $total_cost;?></i>  
+                        </div>  
+                    </div> 
+ 
+                    <!-- All Request Table Card -->
+                    <div class="table-responsive">              
+
+                        <div class="text-lg font-weight-bold text-dark text-uppercase mb-1 "> All Requests</div> 
+ 
                         <table class='table table-bordered'> 
                             <thead>  
                                 <tr>                  
@@ -178,7 +241,7 @@ while ($row1=mysqli_fetch_array($result)) {
                                     $position=10; // Define how many character you want to display.
                                     $readDate= $row1["Requested_Date"];
                                     $Date = substr($readDate, 0, $position); 
-                                        
+                                    
                                     ?>
                                     <tr>   
                                         <td> <?php echo $row1["Ticket_Number"]; ?>  </td>  
@@ -189,24 +252,20 @@ while ($row1=mysqli_fetch_array($result)) {
                                         <td> <?php echo $row1["Request_Status"]; ?>  </td>                                                   
                                         <td> <?php echo $Date; ?>  </td>
 
-                                        <?php 
-                                        
-                                        
-                                        
+                                        <?php  
                                             $services2= mysqli_query($db, "SELECT Cost FROM services WHERE Services = '$row1[Services]' ");
                                             
                                             while ($rowq2=mysqli_fetch_assoc($services2)) {
                                                 $service = mysqli_query($db, "SELECT COUNT(ID), Services FROM maintenancerequest GROUP BY Ticket_Number HAVING COUNT(ID) > 0; ");
                                                     
-                                                while ($rowq=mysqli_fetch_assoc($service)) {
-                                                    $quantity  = $rowq["COUNT(ID)"];
-                                                }
+                                                $rowq=mysqli_fetch_assoc($service);
+                                                $quantity  = $rowq["COUNT(ID)"];
                                                 
                                                 $unitCost = 1 * $rowq2["Cost"];
                                                 $total_item += $quantity;
                                                 $total_cost += $unitCost;
                                             }
-                                            ?>                    
+                                        ?>                    
                                     </tr>
                                     <?php 
                                 }   ?>
@@ -217,172 +276,9 @@ while ($row1=mysqli_fetch_array($result)) {
                             <i class="text-lg alert alert-danger "> Total Item: <?php echo $total_item ; ?></i> 
                             <i class="text-lg alert alert-warning ">Total Cost: <?php echo $total_cost ;?></i>  
                         </div>  
-                    </div>
-
-        
-                    <?php /*
-                    if (isset($_POST["submitemployee"])) { 
-
-                        if ($_POST["assignto"] !== "Select IT Employee"){
-                            $res3 = mysqli_query($db, "SELECT * FROM maintenancerequest WHERE Assigned_To = '$_POST[assignto]' ");
-                            $row = mysqli_fetch_array($res3); 
-                            $row1 = mysqli_num_rows($res3);
-                            
-                            $res = mysqli_query($db, "SELECT * FROM user WHERE User_Namee = '$_POST[assignto]' ");
-                            $row12 = mysqli_fetch_array($res); 
-                            $row13 = mysqli_num_rows($res);
-
-
-                            
-                            $countFormat = mysqli_query($db, "SELECT COUNT(Services) as total FROM maintenancerequest WHERE Assigned_To = '$_POST[assignto]' AND  Services = 'FORMAT' ");
-                            $countFormats = mysqli_fetch_assoc($countFormat); 
-                            $countFormatss = $countFormats['total'];   
-                            
-                            $countHardware = mysqli_query($db, "SELECT COUNT(Services) as total FROM maintenancerequest WHERE Assigned_To = '$_POST[assignto]' AND  Services = 'HARDWARE' ");
-                            $countHardwares = mysqli_fetch_assoc($countHardware); 
-                            $countHardwaress = $countHardwares['total']; 
-
-                            $countNetwork = mysqli_query($db, "SELECT COUNT(Services) as total FROM maintenancerequest WHERE Assigned_To = '$_POST[assignto]' AND  Services = 'NETWORK' ");
-                            $countNetworks = mysqli_fetch_assoc($countNetwork); 
-                            $countNetworkss = $countNetworks['total']; 
-
-                            $countScreen = mysqli_query($db, "SELECT COUNT(Services) as total FROM maintenancerequest WHERE Assigned_To = '$_POST[assignto]' AND  Services = 'SCREEN' ");
-                            $countScreens = mysqli_fetch_assoc($countScreen); 
-                            $countScreenss = $countScreens['total'];  
-
-                            $countSoftware = mysqli_query($db, "SELECT COUNT(Services) as total FROM maintenancerequest WHERE Assigned_To = '$_POST[assignto]' AND  Services = 'SOFTWARE INSTALLATION' ");
-                            $countSoftwares = mysqli_fetch_assoc($countSoftware); 
-                            $countSoftwaress = $countSoftwares['total']; 
-
-                            $countUnknown = mysqli_query($db, "SELECT COUNT(Services) as total FROM maintenancerequest WHERE Assigned_To = '$_POST[assignto]' AND  Services = 'UNKNOWN' ");
-                            $countUnknowns = mysqli_fetch_assoc($countUnknown); 
-                            $countUnknownss = $countUnknowns['total']; 
-                            
-                            $format  =  $countFormatss   * 100 ;
-                            $hardware = $countHardwaress * 100 ;
-                            $network  = $countNetworkss  * 100 ;
-                            $screen   = $countScreenss   * 100 ;
-                            $software = $countSoftwaress * 100 ;
-                            $unknown  = $countUnknownss  * 100 ;
-                            
-                            ?> 
-
-                            <div> 
-                            
-                                <div class="text-lg font-weight-bold text-dark text-uppercase mb-1 text-center"> <?php echo $row12['First_Name'].' '.$row12['Middle_Name']?> </div> 
-
-                                <div class="text-md font-weight-bold text-success text-uppercase mb-4 ">counted <?php echo $row1;?></div>  
-
-                                <p><i class="text-md text-success text-uppercase mb-1" >Format   = </i><i><?php echo $format   ?></i>  </p>
-                                <p><i class="text-md text-success text-uppercase mb-1" >Hardware = </i><i> <?php echo $hardware ?></i> </p>
-                                <p><i class="text-md text-success text-uppercase mb-1" >Network  = </i><i><?php echo $network  ?></i>  </p>
-                                <p><i class="text-md text-success text-uppercase mb-1" >Screens  = </i><i> <?php echo $screen   ?></i> </p>
-                                <p><i class="text-md text-success text-uppercase mb-1" >Software = </i><i> <?php echo $software ?></i> </p>
-                                <p><i class="text-md text-success text-uppercase mb-1" >Unknown  = </i><i><?php echo $unknown  ?></i>  </p>
-                                <p class="text-md text-success text-uppercase mb-1" >Total = <?php echo $format + $hardware + $network + $screen + $software + $unknown?> </p>
-                            </div>
-
-                            <?php
-                            if ($row1 == 0) {
-                                echo '<div class="text-center"> No data has been recorded by '. $_POST['assignto'] .' </div>';
-                            }
-
-                            elseif ($row1 == 1) {  
-                                ?>  
-                        
-                                <div class="card-body "> 
-                                
-                                    <div class="table-responsive"> 
-                                        <table class='table table-bordered'> 
-                                            <thead>  
-                                                <tr>                  
-                                                    <th> Ticket Number </th> 
-                                                    <th> Woreda </th>
-                                                    <th> Services </th>
-                                                    <th> Computer Type </th>  
-                                                    <th> Assigned_To </th> 
-                                                    <th> Request Status </th> 
-                                                    <th> Requested Date </th>  
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php  
-                                                    $position=10; // Define how many character you want to display. 
-                                                    $readDate= $row["Requested_Date"];
-                                                    $Date = substr($readDate, 0, $position); 
-                                                ?> 
-                                                <tr>   
-                                                    <td> <?php echo $row["Ticket_Number"]; ?>  </td>  
-                                                    <td> <?php echo $row["Woreda"]; ?>  </td> 
-                                                    <td> <?php echo $row["Services"]; ?>  </td> 
-                                                    <td> <?php echo $row["Computer_Type"]; ?>  </td>  
-                                                    <td> <?php echo $row["Assigned_To"]; ?>  </td>   
-                                                    <td> <?php echo $row["Request_Status"]; ?>  </td>                                                   
-                                                    <td> <?php echo $Date; ?>  </td>   
-                                                </tr> 
-                                            </tbody>
-                                        </table> 
-                                    </div>
-                                </div>  
-                                <?php
-                            } 
-
-                            elseif ($row1 > 1) {
-                                ?>  
-                                    <div class="table-responsive"> 
-                                        <table class='table table-bordered'> 
-                                            <thead>  
-                                                <tr>                  
-                                                    <th> Ticket Number </th> 
-                                                    <th> Woreda </th>
-                                                    <th> Services </th>
-                                                    <th> Computer Type </th>  
-                                                    <th> Assigned_To </th> 
-                                                    <th> Request Status </th> 
-                                                    <th> Requested Date </th>  
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <?php
-                                                while($row1=mysqli_fetch_array($res3)) {  
-
-                                                    $position=16; // Define how many character you want to display. 
-                                                    $readDate= $row1["Services"];
-                                                    $Services = substr($readDate, 0, $position);
-                                                    
-                                                    $position=10; // Define how many character you want to display. 
-                                                    $readDate= $row1["Requested_Date"];
-                                                    $Date = substr($readDate, 0, $position);
-                                                ?>
-                                                    <tr>   
-                                                        <td> <?php echo $row1["Ticket_Number"]; ?>  </td>  
-                                                        <td> <?php echo $row1["Woreda"]; ?>  </td> 
-                                                        <td> <?php echo $Services; ?>  </td> 
-                                                        <td> <?php echo $row1["Computer_Type"]; ?>  </td>  
-                                                        <td> <?php echo $row1["Assigned_To"]; ?>  </td>   
-                                                        <td> <?php echo $row1["Request_Status"]; ?>  </td>                                                   
-                                                        <td> <?php echo $Date; ?>  </td>   
-                                                    </tr>
-                                                    <?php
-                                                } ?>
-                                            </tbody>
-                                        </table>  
-                                </div>  
-                                <?php
-                            } 
-                            
-                            else{
-                                echo 'ERORRRRR';
-                            }
-                        }
-                        
-                        elseif ($_POST["assignto"] == "Select IT Employee"){
-                            echo "select an employee";
-                        }
-                    } */?>
+                    </div> 
                 </div> 
             </div>
-<<<<<<< HEAD
         </div>
 
         <!-- Content Column -->
@@ -426,9 +322,9 @@ while ($row1=mysqli_fetch_array($result)) {
                     </div>
                 </div>
             </div> 
-=======
->>>>>>> 4fd727baf874207cff1257c2e351ee4379a2f34e
+ 
         </div> 
+
     </div>
 
 </div>
